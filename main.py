deb3b3cbc0bdefb6c1cf5477ab1effb64b726eb1
Mouna LAHRACH <64211381+mounadeev@users.noreply.github.com>
2022-03-31 11:48:34 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#Import the necessary Python library
+import os
+import math
+import random
+import smtplib
+
+digits="0123456789"
+OTP=""
+for i in range(6):
+    OTP+=digits[math.floor(random.random()*10)]
+otp = OTP + " is your OTP"
+message= otp
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("Your Gmail Account", "You app password")
+emailid = input("Enter your email: ")
+s.sendmail('&&&&&&&&&&&',emailid,message)
+a = input("Enter Your OTP >>: ")
+if a == OTP:
+    print("Verified")
+else:
+    print("Please Check your OTP again")
